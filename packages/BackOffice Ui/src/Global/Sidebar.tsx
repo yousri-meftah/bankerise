c0b04a90e893ddc4476699da255208adc46e9a5c
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-03-08 15:18:42 +0100

    SideBar Animation

--- packages/BackOffice Ui/src/Global/Sidebar.tsx
+++ packages/BackOffice Ui/src/Global/Sidebar.tsx
@@ -3,24 +3,33 @@ import { Link } from 'react-router-dom';
 import { HiMenuAlt3 } from "react-icons/hi";
 import { Modules } from '../utils/constants.js'
 import { Logos } from '../utils/constants.js'
+import { FaAngleRight } from "react-icons/fa6";
+import { FaAngleLeft } from "react-icons/fa6";
 const Sidebarr = () => {
+  const [open, setOpen] = React.useState(true)
 
   return (
     <>
-      <div className='bg-gradient-to-b from-[#0d0f1d] to-[#24303f] min-h-full w-72 text-gray-100'>
+      <div className={`bg-gradient-to-b from-[#0d0f1d] to-[#24303f] min-h-full  ${open ? 'w-72' : 'w-16'} duration-500 text-gray-100 relative`}>
+  {/* LOGO HERE */}
+        {open ? <div className='p-3 h-[65px] flex justify-around items-center'>
+          <img src={Logos.logo} alt="Bankerise's Logo" className='hover:cursor-pointer' />
+          <FaAngleLeft color='gray' size={22} className='cursor-pointer' onClick={() => setOpen(!open)} />
+        </div> : <div className='p-3 h-[65px] flex flex-col justify-center items-center'>
+        <img src={Logos.logoxs} alt="Bankerise's Logo" className='hover:cursor-pointer w-[35px]' />
+          <FaAngleRight color='gray' size={22} className='cursor-pointer absolute -right-5' onClick={() => setOpen(!open)} />
+        </div>}
 
-        <div className='p-3 h-[65px] flex justify-around'>
-          <img src={Logos.logo} alt="Bankerise's Logo" className='hover:cursor-pointer'/>
-          <HiMenuAlt3 size={36} className='cursor-pointer' />
-        </div>
-        <div className='mt-8 mx-5 flex flex-col gap-4 relative ' style={{ fontFamily: 'Poppins' }}>
+  {/* ITERATING ON MODULES */}
+        <div className='mt-8 mx-3 flex flex-col gap-4 relative ' style={{ fontFamily: 'Poppins' }}>
           {
             Modules?.map((module, i) => (
               <div key={i} className='flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-gray-700 hover:cursor-pointer rounded-md my-1'>
                 <div>
                   {React.createElement(module?.icon, { size: '20' })}
                 </div>
-                <h2>
+                <h2 style={{transitionDelay:`${i+3}00ms`}} 
+                 className={`whitespace-pre duration-500 ${!open && 'translate-x-20 overflow-hidden opacity-0'}`}>
                   {module?.name}
                 </h2>
               </div>))}
--- packages/BackOffice Ui/src/Global/Topbar.tsx
+++ packages/BackOffice Ui/src/Global/Topbar.tsx
@@ -56,9 +56,9 @@ const Topbar = () => {
                   </button>
 
                   {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-4 flex-shrink-0">
+                  <Menu as="div" className="relative ml-4 flex-shrink-0 ">
                     <div>
-                      <Menu.Button className="flex rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <Menu.Button className="flex rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 ">
                         <span className="sr-only">Open user menu</span>
                         <img
                           className="h-8 w-8 rounded-full"
@@ -76,7 +76,7 @@ const Topbar = () => {
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95"
                     >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-[#24303f] py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ">
                         <Menu.Item>
                           {({ active }) => (
                             <a
