05324944ff2aa91a3713048e5aa75ada36730dd7
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-04-25 17:22:48 +0200

    export button fix

--- packages/BackOffice Ui/src/Components/ExportButton.tsx
+++ packages/BackOffice Ui/src/Components/ExportButton.tsx
@@ -3,7 +3,7 @@ import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { MdOutlineFileUpload } from 'react-icons/md'
 
-function classNames(...classes) {
+function classNames(...classes : any) {
   return classes.filter(Boolean).join(' ')
 }
 
@@ -28,7 +28,7 @@ export default function Example() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-28 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
